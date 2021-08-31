76d2acf1bfdcfa3d2d09700509bd15b4591b3e5c
Jakub Piasecki <jakpia21@gmail.com>
2021-08-31 16:19:18 +0200

    tmp change

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
+from setuptools import dist
+dist.Distribution().fetch_build_eggs(['numpy>=1.13, <=1.19.3'])
+
 import os
 import sys
 import shutil
-from distutils.command.build_ext import build_ext
-from distutils.core import Extension, setup
-from distutils.sysconfig import customize_compiler
+from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
 
 PACKAGE_NAME='bfloat16'
 
-import numpy as np
 
 if 'clean' in sys.argv:
     curdir = os.path.dirname(os.path.realpath(__file__))
@@ -19,18 +20,23 @@ if 'clean' in sys.argv:
                 shutil.rmtree(filepath)
 
 class my_build_ext(build_ext):
+    def finalize_options(self):
+        build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
     def build_extensions(self):
-        customize_compiler(self.compiler)
         try:
             self.compiler.compiler_so.remove("-Wstrict-prototypes")
         except (AttributeError, ValueError):
             pass
         build_ext.build_extensions(self)
 
-
 module1 = Extension(PACKAGE_NAME,
                     sources=['bfloat16.cc'],
-                    include_dirs=[np.get_include()])
+                    extra_compile_args=['-std=c++11'])
 
 setup(name=PACKAGE_NAME,
       version='1.1',
@@ -40,6 +46,7 @@ setup(name=PACKAGE_NAME,
       author_email='support@greenwaves-technologies.com',
       url='https://github.com/GreenWaves-Technologies/bfloat16',
       download_url = 'https://github.com/GreenWaves-Technologies/bfloat16/archive/refs/tags/1.0.tar.gz',
-      install_requires=[],
+      setup_requires=['numpy'],
       ext_modules=[module1],
+      packages=find_packages(),
       cmdclass={'build_ext': my_build_ext})
