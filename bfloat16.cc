c29696281f3cf28987ccbc6554d1abae78d426d2
jakpiase <jakpia21@gmail.com>
2021-09-09 12:11:26 +0200

    minor fix

--- bfloat16.cc
+++ bfloat16.cc
@@ -2083,7 +2083,7 @@ namespace greenwaves
   #define MOD_ERROR_VAL
   #define MOD_SUCCESS_VAL(val)
   #define MOD_INIT(name) void init##name(void)
-  #define MOD_DEF(ob, name, methods) \-
+  #define MOD_DEF(ob, name, methods) \
 		  ob = Py_InitModule3(name, methods, NULL);
 #endif
 
